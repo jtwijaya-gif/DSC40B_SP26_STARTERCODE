f800c54c8cbf9fa416d531755ae0018ff51b1c65
Joseph Timmy Indra Wijaya <jtimiw05@gmail.com>
2026-05-07 21:17:02 -0700

    Add HW5 mode solution

--- hw4/knn_distance.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def knn_distance(arr, q, k):
-    """Compute the kth nearest point and the distance to it."""
-    # TODO: Implement the knn_distance function
-
-    return None
--- hw5/mode.py
+++ hw5/mode.py
@@ -1,7 +1,21 @@
 def mode(numbers):
-    '''
+    """
     Efficiently finds the mode of a list of numbers.
-    '''
-    #TODO: Implement the mode function
+    """
 
-    return None
+    counts = {}
+
+    best = None
+    best_count = 0
+
+    for x in numbers:
+        if x in counts:
+            counts[x] += 1
+        else:
+            counts[x] = 1
+
+        if counts[x] > best_count:
+            best = x
+            best_count = counts[x]
+
+    return best
